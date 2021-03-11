5a34fb0c9d0674801820ed626f6a115683104269
svesk <sdp.svensson@gmail.com>
2021-03-11 16:45:51 +0100

    Initial commit

---
+++ src/test/java/com/trial/springtrial/SpringTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trial.springtrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
